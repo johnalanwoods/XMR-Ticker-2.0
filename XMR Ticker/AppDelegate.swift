03b1cf028d5830400ba4cfe9de41f717bd3d8b0b
johnalanwoods <john.alan.woods@gmail.com>
2017-01-14 19:54:35 +0000

    Initial Commit

---
+++ XMR Ticker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XMR Ticker
+//
+//  Created by John Woods on 14/01/2017.
+//  Copyright © 2017 John Woods. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
